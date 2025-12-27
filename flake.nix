ca6f0074c11f12fe14ae4ce4d9a2f5b1b0c9551a
Arthur Heymans <arthur@aheymans.xyz>
2025-12-27 20:36:01 +0100

    Add cross-compilation support for i686, aarch64, and armv7
    Refactor flake.nix with a data-driven approach for cross-compilation
    targets. This eliminates code duplication and makes adding new
    architectures trivial.
    
    - Add crossTargets attribute set defining supported architectures
    - Create helper functions (mkCrossPkgs, mkBuildInputs, mkCrossDevShell,
      mkCrossPackage) to reduce duplication
    - Generate cross dev shells and packages programmatically
    - Support i686-unknown-linux-gnu, aarch64-unknown-linux-gnu, and
      armv7-unknown-linux-gnueabihf targets

--- flake.nix
+++ flake.nix
@@ -17,74 +17,153 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        
+        lib = pkgs.lib;
+
+        # Cross-compilation targets: name -> { config, rustTarget, cargoLinkerEnv }
+        crossTargets = {
+          i686 = {
+            config = "i686-unknown-linux-gnu";
+            rustTarget = "i686-unknown-linux-gnu";
+            cargoLinkerEnv = "CARGO_TARGET_I686_UNKNOWN_LINUX_GNU_LINKER";
+          };
+          aarch64 = {
+            config = "aarch64-unknown-linux-gnu";
+            rustTarget = "aarch64-unknown-linux-gnu";
+            cargoLinkerEnv = "CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER";
+          };
+          armv7 = {
+            config = "armv7l-unknown-linux-gnueabihf";
+            rustTarget = "armv7-unknown-linux-gnueabihf";
+            cargoLinkerEnv = "CARGO_TARGET_ARMV7_UNKNOWN_LINUX_GNUEABIHF_LINKER";
+          };
+        };
+
+        # Generate cross pkgs for each target
+        mkCrossPkgs = target: import nixpkgs {
+          inherit system overlays;
+          crossSystem.config = target.config;
+        };
+
+        # Build inputs as a function of pkgs
+        mkBuildInputs = p: with p; [
+          udev
+          libftdi1
+          pciutils
+        ];
+
+        # Base rust toolchain
         rustToolchain = pkgs.rust-bin.stable.latest.default.override {
           extensions = [ "rust-src" "rust-analyzer" ];
         };
-        
-        # Common build inputs for the project
-        buildInputs = with pkgs; [
-          # USB support
-          # libusb1
-          
-          # Serial port support (requires udev on Linux)
-          udev
-          
-          # For FTDI programmers (future)
-          libftdi1
-          
-          # For internal programmer (future)
-          pciutils
-        ];
-        
-        nativeBuildInputs = with pkgs; [
-          pkg-config
-          rustToolchain
-          cargo
-        ];
-        
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          inherit buildInputs nativeBuildInputs;
-          
-          # Environment variables for pkg-config to find libraries
-          PKG_CONFIG_PATH = pkgs.lib.makeSearchPath "lib/pkgconfig" buildInputs;
-          
-          # For libudev
-          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-          
-          shellHook = ''
-            echo "rflasher development environment"
-            echo "Rust version: $(rustc --version)"
-            echo ""
-            echo "Available commands:"
-            echo "  cargo build              - Build the project"
-            echo "  cargo test               - Run tests"
-            echo "  cargo run -- --help      - Show CLI help"
-            echo ""
-          '';
+
+        # Rust toolchain with cross targets
+        rustToolchainCross = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+          targets = lib.mapAttrsToList (_: t: t.rustTarget) crossTargets;
         };
-        
-        packages.default = pkgs.rustPlatform.buildRustPackage {
-          pname = "rflasher";
-          version = "0.1.0";
-          
-          src = ./.;
-          
-          cargoLock = {
-            lockFile = ./Cargo.lock;
+
+        # Create a cross-compilation dev shell for a given target
+        mkCrossDevShell = name: target:
+          let
+            crossPkgs = mkCrossPkgs target;
+            crossBuildInputs = mkBuildInputs crossPkgs;
+          in pkgs.mkShell {
+            buildInputs = crossBuildInputs;
+            nativeBuildInputs = [ pkgs.pkg-config rustToolchainCross ];
+
+            PKG_CONFIG_PATH = lib.makeSearchPath "lib/pkgconfig" crossBuildInputs;
+            PKG_CONFIG_SYSROOT_DIR = "${crossPkgs.stdenv.cc.libc}";
+            LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+
+            "${target.cargoLinkerEnv}" = "${crossPkgs.stdenv.cc}/bin/${crossPkgs.stdenv.cc.targetPrefix}cc";
+
+            shellHook = ''
+              echo "rflasher cross-compilation environment (${target.rustTarget})"
+              echo "Rust version: $(rustc --version)"
+              echo ""
+              echo "Build with:"
+              echo "  cargo build --target ${target.rustTarget}"
+              echo ""
+            '';
           };
-          
-          inherit buildInputs nativeBuildInputs;
-          
-          meta = with pkgs.lib; {
-            description = "A modern Rust port of flashprog for reading, writing, and erasing flash chips";
-            homepage = "https://github.com/user/rflasher";
-            license = licenses.gpl2Plus;
-            maintainers = [ ];
+
+        # Create a cross-compiled package for a given target
+        mkCrossPackage = name: target:
+          let
+            crossPkgs = mkCrossPkgs target;
+          in crossPkgs.rustPlatform.buildRustPackage {
+            pname = "rflasher";
+            version = "0.1.0";
+            src = ./.;
+
+            cargoLock.lockFile = ./Cargo.lock;
+
+            buildInputs = mkBuildInputs crossPkgs;
+            nativeBuildInputs = [ pkgs.pkg-config rustToolchainCross ];
+
+            CARGO_BUILD_TARGET = target.rustTarget;
+
+            meta = with lib; {
+              description = "A modern Rust port of flashprog for reading, writing, and erasing flash chips";
+              homepage = "https://github.com/user/rflasher";
+              license = licenses.gpl2Plus;
+            };
           };
-        };
+
+        # Generate all cross dev shells and packages
+        crossDevShells = lib.mapAttrs' (name: target:
+          lib.nameValuePair "cross-${name}" (mkCrossDevShell name target)
+        ) crossTargets;
+
+        crossPackages = lib.mapAttrs' (name: target:
+          lib.nameValuePair "cross-${name}" (mkCrossPackage name target)
+        ) crossTargets;
+
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = mkBuildInputs pkgs;
+            nativeBuildInputs = [ pkgs.pkg-config rustToolchain pkgs.cargo ];
+
+            PKG_CONFIG_PATH = lib.makeSearchPath "lib/pkgconfig" (mkBuildInputs pkgs);
+            LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+
+            shellHook = ''
+              echo "rflasher development environment"
+              echo "Rust version: $(rustc --version)"
+              echo ""
+              echo "Available commands:"
+              echo "  cargo build              - Build the project"
+              echo "  cargo test               - Run tests"
+              echo "  cargo run -- --help      - Show CLI help"
+              echo ""
+              echo "Cross-compilation shells:"
+              echo "  nix develop .#cross-i686    - i686-unknown-linux-gnu"
+              echo "  nix develop .#cross-aarch64 - aarch64-unknown-linux-gnu"
+              echo "  nix develop .#cross-armv7   - armv7-unknown-linux-gnueabihf"
+              echo ""
+            '';
+          };
+        } // crossDevShells;
+
+        packages = {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "rflasher";
+            version = "0.1.0";
+            src = ./.;
+
+            cargoLock.lockFile = ./Cargo.lock;
+
+            buildInputs = mkBuildInputs pkgs;
+            nativeBuildInputs = [ pkgs.pkg-config rustToolchain pkgs.cargo ];
+
+            meta = with lib; {
+              description = "A modern Rust port of flashprog for reading, writing, and erasing flash chips";
+              homepage = "https://github.com/user/rflasher";
+              license = licenses.gpl2Plus;
+            };
+          };
+        } // crossPackages;
       }
     );
 }
